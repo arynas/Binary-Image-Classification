efad95dd51b0752fe6e8bb5a0e81bee3408230b5
Moch. Ari Nasichuddin <arynas92@gmail.com>
2018-07-22 07:05:10 +0700

    Add predict file

---
+++ predict.py
@@ -0,0 +1,23 @@
+import numpy as np
+from keras.preprocessing import image
+from keras.models import model_from_yaml
+
+test_image = image.load_img('data/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+
+#load model
+yaml_file = open('model.yaml', 'r')
+loaded_model_yaml = yaml_file.read()
+yaml_file.close()
+model = model_from_yaml(loaded_model_yaml)
+model.load_weights("model.h5")
+
+result = model.predict(test_image)
+
+
+# training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
