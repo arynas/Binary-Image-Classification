8dbb3727fb6ef928ce3f89356fece0755fa8e50d
atmatech <atmatech@Tesla.localdomain>
2018-07-22 07:33:24 +0700

    After predict

--- predict.py
+++ predict.py
@@ -2,7 +2,7 @@ import numpy as np
 from keras.preprocessing import image
 from keras.models import model_from_yaml
 
-test_image = image.load_img('data/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.load_img('data/test_set/cats/cat.4714.jpg', target_size = (64, 64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 
@@ -17,9 +17,10 @@ result = model.predict(test_image)
 
 
 # training_set.class_indices
+print(result)
 if result[0][0] == 1:
     prediction = 'dog'
 else:
     prediction = 'cat'
 
-print("The prediction is ", prediction)
+print("The prediction is", prediction)
